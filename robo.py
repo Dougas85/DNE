dcf4c8a36a9c79a8ea8191628d5f311a323186c6
DOUGLAS FRANCISCO DA SILVA <149700231+Dougas85@users.noreply.github.com>
2025-06-16 14:31:17 -0300

    Update robo.py

--- robo.py
+++ robo.py
@@ -182,4 +182,5 @@ def processar_excel(path):
 
 if __name__ == '__main__':
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
